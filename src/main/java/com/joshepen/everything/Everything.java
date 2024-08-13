f847b9b5582791a4119e33d918011d89d77958ee
Josh <joshuaepenner@gmail.com>
2024-08-12 19:46:19 -0500

    Initial commit

---
+++ src/main/java/com/joshepen/everything/Everything.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.joshepen.everything;
+
+/**
+ *
+ * @author joshu
+ */
+public class Everything {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new UI();
+    }
+}
