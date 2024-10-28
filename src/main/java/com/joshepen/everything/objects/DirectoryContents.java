98d755690b227ee48aa86f0ce0f4b4f5bf5b1fac
joshepen <119086370+joshepen@users.noreply.github.com>
2024-10-27 20:48:21 -0500

    Add recursive search code

--- src/main/java/com/joshepen/everything/objects/DirectoryContents.java
+++ src/main/java/com/joshepen/everything/objects/DirectoryContents.java
@@ -4,23 +4,49 @@ import java.io.File;
 import java.util.*;
 
 public class DirectoryContents {
-    File dir;
-    File[] files;
-    ArrayList<File> processedFiles;
-    String searchTerm;
-    boolean caseSensitive;
+    private final int MAX_NUM_DIRS = 3;
+    private File dir;
+    private ArrayList<File> files;
+    private ArrayList<File> processedFiles;
+    private String searchTerm;
+    private boolean caseSensitive;
+    private boolean recursive;
     
     public DirectoryContents(){
         searchTerm = "";
         caseSensitive = false;
         setDirectory("C:\\");
         refreshFiles();
+        recursive = false;
     }
 
     public void refreshFiles(){
-        files = dir.listFiles();
+        files = getFiles(dir, 1);
         searchName(searchTerm);
         sort();
+    }
+
+    public ArrayList<File> getFiles(File dir, int numDirs){
+        File[] currFiles = dir.listFiles();
+        ArrayList<File> outFiles = new ArrayList<>();
+        if(currFiles != null){
+            for(File file : currFiles){
+                if (file.isFile()){
+                    outFiles.add(file);
+                }
+            }
+
+            // I need breadth first traversal so I have to loop a second time for dirs
+            if(recursive){
+                for(File file : currFiles){
+                    if(file.isDirectory() && numDirs < MAX_NUM_DIRS){
+                        outFiles.addAll(getFiles(file, numDirs + 1));
+                    }
+                }
+            }
+            
+        }
+        return outFiles;
     }
 
     public void setSearchTerm(String term){
@@ -29,6 +55,10 @@ public class DirectoryContents {
 
     public void setDirectory(String path){
         dir = new File(path);
+    }
+
+    public void setRecursive(boolean recursive){
+        this.recursive = recursive;
     }
 
     public void setCaseSensitive(boolean caseSensitive) {
@@ -62,12 +92,12 @@ public class DirectoryContents {
         if(!caseSensitive) term = term.toLowerCase();
         processedFiles = new ArrayList<>();
         String currFileName;
-        for(int i=0;i<files.length;i++){
-            currFileName = files[i].getName();
+        for(int i=0;i<files.size();i++){
+            currFileName = files.get(i).getName();
             if(!caseSensitive) currFileName = currFileName.toLowerCase();
 
             if(currFileName.contains(term)){
-                processedFiles.add(files[i]);
+                processedFiles.add(files.get(i));
             }
         }
     }
